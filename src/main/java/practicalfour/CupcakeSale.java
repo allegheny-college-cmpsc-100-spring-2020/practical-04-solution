b8ac1aea6e1b0da53246b4efa9ab644b5eef922e
Douglas Luman <dluman@douglasjluman.com>
2020-02-14 09:20:48 -0500

    Initial commit

---
+++ src/main/java/practicalfour/CupcakeSale.java
@@ -0,0 +1,51 @@
+package practicalfour;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/** Displays the totals of a cupcake sale.
+ *
+ * @author Solution Repository
+ */
+public class CupcakeSale {
+
+  /** Entry point.
+   *
+   * @param args The command line arguments
+   */
+  public static void main(String[] args) {
+    // Set up scanner and read values from file
+    File file = null;
+    Scanner input = null;
+    try {
+      file = new File("../../../../input/cupcakes.nomnomnom");
+      input = new Scanner(file);
+    } catch (FileNotFoundException noFile) {
+      System.exit(0);
+    }
+    String strawberrySales = input.next();
+    input.nextLine();
+    String redVelvetSales = input.next();
+    input.nextLine();
+    String raspberrySales = input.next();
+    
+    // Calculate sales
+    double taxRate = .08;
+    int strawberryTotal = strawberrySales.length();
+    int redVelvetTotal = redVelvetSales.length();
+    int raspberryTotal = raspberrySales.length();
+    
+    // Calculate total
+    double total =  strawberryTotal + redVelvetTotal + raspberryTotal;
+    double tax = taxRate * (strawberryTotal + redVelvetTotal + raspberryTotal);
+    
+    // Print total
+    System.out.println(strawberryTotal + "\tstrawberry cupcakes");
+    System.out.println(redVelvetTotal + "\tred velvet cupcakes");
+    System.out.println(raspberryTotal + "\traspberry cupcakes");
+    System.out.println((int)(taxRate * 100) + "%\tsales tax");
+    System.out.println("----");
+    System.out.print((total + tax));
+  }
+}
---
+++ writing/reflection.md
@@ -0,0 +1,24 @@
+# Lab 5 reflection: {Your Name Here}
+
+TODO: Write reflection
+
+## List the methods of `String` used in this lab. What did each do, and how did you use them? What types of data did they return?
+
+
+## Reflect on the `char` primitive. How did the data type make it easier to shift letters?
+
+
+### Paste _one_ instance of code you used to "shift" a character in the code block below. How did this shift work?
+
+```java
+
+```
+
+## What is `casting` and how does it work?
+
+
+## What was the function of `codeName` in this program?
+
+
+## Describe why the program guidelines asked you to store the encoded word's length in the `key` identifier.
+
